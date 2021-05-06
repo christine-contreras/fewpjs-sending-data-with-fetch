2e8c46fa0233e5ac5e1a13e140dd430bc5e3a216
Christine Contreras <christineec23@yahoo.com>
2021-05-06 16:12:34 -0500

    passing POST lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+// write a function, submitData, that takes two strings as arguments, one representing a user's name and the other representing a user's email.
+function submitData(nameValue, emailValue) {
+    const body = document.body;
+    const data = {
+        name: `${nameValue}`,
+        email: `${emailValue}`
+    }
+
+    const config = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+
+    return fetch('http://localhost:3000/users', config)
+    .then(response => response.json())
+    .then(json => {
+        //console.log(json);
+        const iDTag = document.createElement('p');
+        iDTag.innerHTML = json.id;
+        body.appendChild(iDTag);
+        //debugger;
+
+    })
+    .catch(error => {
+        const errorTag = document.createElement('p');
+        errorTag.innerHTML = error.message;
+        body.appendChild(errorTag);
+
+    });
+}
+
+//submitData('Steve', 'steve@steve.com');
